c266cd816ba7fa16352d86f63f7081ae7ae5f149
lidf <jackdaw17@163.com>
2025-06-23 17:37:45 +0800

    基于 stdio 的 MCP 客户端和服务端的实现

---
+++ my-weather-server-stdio/src/test/java/com/jackdaw/mcp/MyWeatherServerStdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackdaw.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWeatherServerStdioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-ai-bailian/src/main/java/com/jackdaw/springai/controller/MCPController.java
@@ -0,0 +1,38 @@
+package com.jackdaw.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mcp")
+public class MCPController {
+
+    private final ChatClient dashScopeChatClient;
+
+    public MCPController(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools) {
+        this.dashScopeChatClient = chatClientBuilder
+
+                .defaultTools(tools)
+                .build();
+    }
+
+
+
+    @GetMapping("/weather/stdio")
+    public String noTool(@RequestParam(value = "query", defaultValue = "上海今天的天气") String query) {
+
+        System.out.println("\n>>> QUESTION: " + query);
+        String content = dashScopeChatClient.prompt(query).call().content();
+
+        System.out.println("\n>>> ASSISTANT: " + content);
+        return content;
+    }
+
+
+
+
+}
